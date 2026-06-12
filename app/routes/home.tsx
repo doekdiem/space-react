1dc262a200247b62cbce4682d30a13a4188f465e
Rifat Alfasya <rifatalfasya.eng@gmail.com>
2026-06-13 04:37:54 +0800

    add some element

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -9,5 +9,10 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <div>
+      <h1>DOEKDIEM</h1>
+      <Welcome />
+    </div>
+  );
 }
